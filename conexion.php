e1c00d391dc2a6501366cd9ca466ba75e0f44118
AlberthoReyes <alberto-reyes@live.com.mx>
2016-12-07 20:19:33 -0600

    modificacion2

--- conexion.php
+++ conexion.php
@@ -1,8 +1,9 @@
 <?php
-$server="localhost";
-$usuario="root";
-$password="";
-$baseDatos="alimentos_dif";
+
+$usario="u174280093_rbd";
+$passwd="hsa5V9Kcbm";
+$servidor="mysql.hostinger.mx";
+$BD="u174280093_dif";
 
 $conexion=mysql_connect($server, $usuario, $password) or die("Error al conectarse al servidor de base de datos".mysql_error());
 mysql_select_db($baseDatos,$conexion);
